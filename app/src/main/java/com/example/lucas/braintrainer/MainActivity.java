55e47c59080ef67648a627c3b233fe8b37aadecc
Lucas Bisca <lucas.simon.b@gmail.com>
2017-06-11 18:36:07 +0000

    Brain Trainer Third Commit

--- app/src/main/java/com/example/lucas/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/lucas/braintrainer/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.lucas.braintrainer;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ActivityChooserView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,20 +21,60 @@ public class MainActivity extends AppCompatActivity {
     Button button1;
     Button button2;
     Button button3;
+    Button playAgain;
     TextView sumTextView;
     TextView resultTextView;
     TextView scoreView;
+    TextView timer;
+    RelativeLayout gameLayout;
+
     ArrayList<Integer> answers = new ArrayList<Integer>();
     int locationOfCorrectAnswer;
     int numberOfQuestions;
     int score;
 
 
+    public void playAgain(final View view) {
+
+        score = 0;
+        numberOfQuestions = 0;
+
+        timer.setText("30s");
+        scoreView.setText("0/0");
+        resultTextView.setText("");
+        playAgain.setVisibility(View.INVISIBLE);
+        generateQuestion();
+
+        new CountDownTimer(30000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                timer.setText(String.valueOf(millisUntilFinished / 1000) + "s");
+                score = 0;
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                timer.setText("0s");
+                resultTextView.setText("Done!");
+                playAgain.setVisibility(View.VISIBLE);
+
+            }
+        }.start();
+    }
+
     public void start(View view) {
 
         start.setVisibility(View.INVISIBLE);
+        gameLayout.setVisibility(View.VISIBLE);
+
+        playAgain(findViewById(R.id.playAgain));
 
     }
+
 
     public void generateQuestion() {
 
@@ -101,16 +144,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        start = (Button) findViewById(R.id.button);
+        start = (Button) findViewById(R.id.startButton);
         sumTextView = (TextView) findViewById(R.id.label);
         button = (Button) findViewById(R.id.button);
         button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
         button3 = (Button) findViewById(R.id.button3);
+        playAgain = (Button) findViewById(R.id.playAgain);
         resultTextView = (TextView) findViewById(R.id.bottomMsg);
         scoreView = (TextView) findViewById(R.id.score);
+        timer = (TextView) findViewById(R.id.timer);
+        gameLayout = (RelativeLayout) findViewById(R.id.gameLayout);
 
-        generateQuestion();
+
 
     }
 }
